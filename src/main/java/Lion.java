c6340c4d0cc4c00e7e6d5e583f40a9511930b29f
Luyanda2020 <mthura2214@gmail.com>
2020-08-11 16:37:41 +0200

    now i am on fire

---
+++ src/main/java/Lion.java
@@ -0,0 +1,29 @@
+public class Lion extends Animal {
+
+    private String sex;
+
+    public Lion() {
+        super();
+    }
+
+    public Lion(int age, String name, double height, String color, String sex) {
+        this.sex = sex;
+
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+
+        this.sex = sex;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() +"Lion{" +
+                "sex='" + sex + '\'' +
+                '}';
+    }
+}
